32a4a89dcdd21e8db49ca769329881c4356df7b4
Sabnit <sabnit111@gmail.com>
2025-05-06 22:57:13 +0545

    refactor: replace hardcoded link with Butoon component in  NotFound page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
+import Button from "../components/atoms/Button";
 import { CLIENT_ROUTES } from "../constants/clientRoutes";
 
 const NotFound = () => {
@@ -7,12 +9,11 @@ const NotFound = () => {
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100 px-4">
       <h1 className="text-6xl font-bold text-gray-800 mb-4">404</h1>
       <p className="text-xl text-gray-600 mb-6">Oops! Page not found.</p>
-      <Link
-        to={CLIENT_ROUTES.WELCOME}
-        className="px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
-      >
-        Back to Welcome Page
-      </Link>
+      <>
+        <Link to={CLIENT_ROUTES.WELCOME}>
+          <Button>Back to Welcome Page</Button>
+        </Link>
+      </>
     </div>
   );
 };
