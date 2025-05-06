3f9c8c07285bf0b346d3d8fef9168593d492675c
Sabnit <sabnit111@gmail.com>
2025-05-07 00:38:08 +0545

    refactor: add 'none' variant to Button component

--- frontend/src/components/atoms/Button.jsx
+++ frontend/src/components/atoms/Button.jsx
@@ -4,7 +4,7 @@ import React from "react";
  * Custom Button component
  *
  * @param {object} props
- * @param {'primary' | 'secondary' | 'danger' | 'simple' | 'note'} [props.variant] - Visual style of the button
+ * @param {'primary' | 'secondary' | 'danger' | 'simple' | 'note' | 'none'} [props.variant] - Visual style of the button
  * @param {'button' | 'submit' | 'reset'} [props.type] - Type of the button
  * @param {string} [props.className] - Additional class names
  * @param {boolean} [props.disabled] - Disable the button
@@ -20,17 +20,18 @@ const Button = ({
   onClick,
 }) => {
   const baseStyles =
-    "py-2 px-4 font-medium focus:outline-none focus:ring-1 transition-colors cursor-pointer";
+    "py-2 px-4 font-medium focus:outline-none focus:ring-1 transition-colors cursor-pointer rounded disabled:opacity-50 disabled:cursor-not-allowed";
 
   const variants = {
     primary:
-      "bg-orange-400 hover:bg-orange-500 text-white focus:ring-blue-300 rounded",
+      "bg-orange-400 hover:bg-orange-500 text-white focus:ring-blue-300 ",
     secondary:
-      "bg-gray-200 hover:bg-gray-300 text-white focus:ring-gray-300 rounded",
-    danger: "bg-red-600 hover:bg-red-700 text-white focus:ring-red-300",
+      "bg-gray-200 hover:bg-gray-300 text-gray-600 focus:ring-gray-300 ",
+    danger: "bg-red-400 hover:bg-red-500 text-white focus:ring-red-300",
     simple:
       "flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all",
     note: "hover:bg-gray-200 flex items-center gap-2 font-medium cursor-pointer",
+    none: "",
   };
 
   return (
