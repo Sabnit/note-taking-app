0e30381b1269f2fa64b58ed693ec9bd40cdd0257
Sabnit <sabnit111@gmail.com>
2025-05-15 14:54:58 +0545

    refactor: add note, category and appUI context providers

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,24 +1,31 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
-import { StrictMode } from "react";
-import { AuthProvider } from "./context/AuthContext.jsx";
 import { ToastContainer } from "react-toastify";
-import { AppProvider } from "./context/AppContext.jsx";
+import { BrowserRouter } from "react-router-dom";
+
+import "./index.css";
+
+import App from "./App.jsx";
+
 import QueryProvider from "./context/QueryProvider.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import { AppUIProvider } from "./context/AppUIContext.jsx";
+import { NoteProvider } from "./context/NoteContext.jsx";
+import { CategoryProvider } from "./context/CategoryContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <QueryProvider>
-        <AuthProvider>
-          <AppProvider>
-            <ToastContainer />
-            <App />
-          </AppProvider>
-        </AuthProvider>
-      </QueryProvider>
-    </BrowserRouter>
-  </StrictMode>
+  <BrowserRouter>
+    <QueryProvider>
+      <AuthProvider>
+        <AppUIProvider>
+          <NoteProvider>
+            <CategoryProvider>
+              <ToastContainer />
+              <App />
+            </CategoryProvider>
+          </NoteProvider>
+        </AppUIProvider>
+      </AuthProvider>
+    </QueryProvider>
+  </BrowserRouter>
 );
